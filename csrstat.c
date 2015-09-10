bad58ffe8ba34ce51c7f2c92a13b21d79e5ba1e8
Pike <PikeRAlpha@yahoo.com>
2015-09-10 15:56:43 +0200

    Cleaner code

--- csrstat.c
+++ csrstat.c
@@ -4,12 +4,16 @@
  * Author.: Pike R. Alpha
  * Purpose: Command line tool for El Capitan to get the active SIP status.
  *
- * Compile with: cc csrstat.c /usr/lib/libSystem.dylib -o csrstat
+ * Compile with: cc csrstat.c /usr/lib/libSystem.B.dylib -o csrstat
+ * 
+ * Updates:
+ *			- Use csr_check so that csr_allow_all/internal are taken into account (Pike R. Alpha September 2015).
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint32_t csr_config_t;
 
@@ -30,7 +34,33 @@ typedef uint32_t csr_config_t;
 	CSR_ALLOW_UNRESTRICTED_DTRACE | \
 	CSR_ALLOW_UNRESTRICTED_NVRAM)
 
+/* Syscalls */
+extern int csr_check(csr_config_t mask);
 extern int csr_get_active_config(csr_config_t *config);
+
+//==============================================================================
+
+char * _csr_check(aMask, aFlipflag)
+{
+	bool stat = 0;
+
+	// Syscall
+	if (csr_check(aMask) != 0)
+	{
+		stat = (aFlipflag) ? 0 : 1;
+	}
+	else
+	{
+		stat = (aFlipflag) ? 1 : 0;
+	}
+	
+	if (stat)
+	{
+		return("enabled");
+	}
+
+	return("\33[1mdisabled\33[0m");
+}
 
 
 //==============================================================================
@@ -38,7 +68,7 @@ extern int csr_get_active_config(csr_config_t *config);
 int main(int argc, const char * argv[])
 {
 	uint32_t config = 0;
-
+	// Syscall
 	csr_get_active_config(&config);
 
 	printf("System Integrity Protection status: %s ", (config == CSR_VALID_FLAGS) ? "\33[1mdisabled\33[0m": "enabled");
@@ -57,12 +87,12 @@ int main(int argc, const char * argv[])
 	
 	printf("\n\nConfiguration:\n");
 
-	printf("\tApple Internal: %s\n", (config & CSR_ALLOW_APPLE_INTERNAL) ? "enabled" : "\33[1mdisabled\33[0m");
-	printf("\tKext Signing Restrictions: %s\n", (config & CSR_ALLOW_UNTRUSTED_KEXTS) ? "\33[1mdisabled\33[0m" : "enabled");
-	printf("\tFilesystem Protections: %s\n", (config & CSR_ALLOW_TASK_FOR_PID) ? "\33[1mdisabled\33[0m" : "enabled");
-	printf("\tDebugging Restrictions: %s\n", (config & CSR_ALLOW_KERNEL_DEBUGGER) ? "\33[1mdisabled\33[0m" : "enabled");
-	printf("\tDTrace Restrictions: %s\n", (config & CSR_ALLOW_UNRESTRICTED_DTRACE) ? "\33[1mdisabled\33[0m" : "enabled");
-	printf("\tNVRAM Protections: %s\n", (config & CSR_ALLOW_UNRESTRICTED_NVRAM) ? "\33[1mdisabled\33[0m" : "enabled");
+	printf("\tApple Internal: %s\n", _csr_check(CSR_ALLOW_APPLE_INTERNAL, 1));
+	printf("\tKext Signing Restrictions: %s\n", _csr_check(CSR_ALLOW_UNTRUSTED_KEXTS, 0));
+	printf("\tFilesystem Protections: %s\n", _csr_check(CSR_ALLOW_UNRESTRICTED_FS, 0));
+	printf("\tDebugging Restrictions: %s\n", _csr_check(CSR_ALLOW_KERNEL_DEBUGGER, 0));
+	printf("\tDTrace Restrictions: %s\n", _csr_check(CSR_ALLOW_UNRESTRICTED_DTRACE, 0));
+	printf("\tNVRAM Protections: %s\n", _csr_check(CSR_ALLOW_UNRESTRICTED_NVRAM, 0));
 	
 	if (config && (config != CSR_ALLOW_APPLE_INTERNAL))
 	{
